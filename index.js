62977ddc68df31ff5447b11c09a5ea1cecc6c69c
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-05 16:02:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import merhaba, { topla, cikar, text, user, users } from "./my-modules.js";
+
+console.log(topla(2, 4));
+console.log(cikar(5, 1));
+console.log(text);
+console.log(user);
+console.log(users);
+merhaba("harun");
---
+++ my-module.js
@@ -0,0 +1,23 @@
+const hello = (name) => {
+  console.log(`hello ${name}`);
+};
+
+export const topla = (a, b) => a + b;
+export const cikar = (a, b) => a - b;
+export const text = "text";
+export const user = {
+  name: "harun",
+  surname: "hatib",
+};
+
+export const users = [
+  {
+    name: "harun",
+    surname: "hatib",
+  },
+  {
+    name: "harun",
+    surname: "hatib",
+  },
+];
+export default hello;
